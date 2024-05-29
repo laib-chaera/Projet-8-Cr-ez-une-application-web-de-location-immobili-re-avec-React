a5bc2b66a13e20a3859e384b5600d32c94212fa0
chaera laib <laibchaera@outlook.fr>
2024-05-29 23:15:38 +0200

    mise à jour des Routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,24 +1,29 @@
-// import React from "react"
-// import { createRoot } from "react-dom"
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-// import Accueil from "../src/pages/accueil/accueil.jsx"
-// import APropos from "../src/pages/aPropos/aPropos.jsx"
-// import Logement from "../src/pages/logement/logement.jsx"
-// import NotFound from "./pages/notFound/notFound.jsx"
-// import Header from "../src/composants/header/header.jsx"
-// import Footer from "../src/composants/footer/footer.jsx"
+import React from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Accueil from "../../src/pages/accueil/accueil.jsx"
+import APropos from "../../src/pages/aPropos/aPropos.jsx"
+import Logement from "../../src/pages/logement/logement.jsx"
+import NotFound from "../../src/pages/notFound/notFound.jsx"
+import Header from "../../src/composants/header/header.jsx"
+import Footer from "../../src/composants/footer/footer.jsx"
 
-// createRoot(document.getElementById("root")).render(
-//     <React.StrictMode>
-//         <Router>
-//             <Header />
-//             <Routes>
-//                 <Route path="/" element={<Accueil />} />
-//                 <Route path="/aPropos" element={<APropos />} />
-//                 <Route path="/logement" element={<Logement />} />
-//                 <Route path="/notfound" element={<NotFound />} />
-//             </Routes>
-//             <Footer />
-//         </Router>
-//     </React.StrictMode>,
-// )
+function AllRoutes() {
+    return (
+        <div>
+            <React.StrictMode>
+                <Router>
+                    <Header />
+                    <Routes>
+                        <Route path="/" element={<Accueil />} />
+                        <Route path="/aPropos" element={<APropos />} />
+                        <Route path="/logement" element={<Logement />} />
+                        <Route path="/notfound" element={<NotFound />} />
+                    </Routes>
+                    <Footer />
+                </Router>
+            </React.StrictMode>
+        </div>
+    )
+}
+
+export default AllRoutes
